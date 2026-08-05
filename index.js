9963606d7ab5f7295a8eb8c20b95cfda1cb922fe
Jenna LoBasso <123016696+jenna-lobasso@users.noreply.github.com>
2026-08-05 12:43:58 -0400

    Fetch and display projects from JSON file

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-console.log('Welcome to JennaOS');
+fetch("projects.json")
+
+.then(response => response.json())
+
+.then(projects => {
+
+
+const container =
+document.getElementById("project-container");
+
+
+projects.forEach(project => {
+
+
+let tags = project.tools.map(
+tool => `<span class="tag">${tool}</span>`
+).join("");
+
+
+
+container.innerHTML += `
+
+
+<div class="project-card">
+
+
+<img src="${project.image}">
+
+
+<h3>
+${project.title}
+</h3>
+
+
+<p>
+${project.description}
+</p>
+
+
+<div>
+${tags}
+</div>
+
+
+<a class="button" href="${project.link}">
+OPEN FILE
+</a>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+});
