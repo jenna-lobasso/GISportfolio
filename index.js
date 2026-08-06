bb3a954490f2789bf3d6fb45e561fcf750716f12
Jenna LoBasso <123016696+jenna-lobasso@users.noreply.github.com>
2026-08-06 12:43:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-fetch("projects.json")
-.then(response => response.json())
-.then(projects => {
-  const container = document.getElementById("project-container");
-  projects.forEach(project => {
-    let tags = project.tools.map(
-      tool => `<span class="tag">${tool}</span>`
-    ).join("");
-    container.innerHTML += `
-      <div class="project-card">
-        <img src="${project.image}">
-        <h3>${project.title}</h3>
-        <p>${project.description}</p>
-        <div>${tags}</div>
-        <a class="button" href="${project.link}">OPEN FILE</a>
-      </div>
-    `;
-  });
-});
+
 
 // --- Windows menu bar ---
 const menubar = document.getElementById('menubar');
